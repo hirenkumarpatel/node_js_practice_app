446dff3e7f7ef26c5973f88bcb88ae41e1c4f8df
Hiren Patel <hirenkumarbpatel@gmail.com>
2019-08-05 19:10:46 -0400

    simple we app ready

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * creating basic server and start running server
+ * to use server that automatically restart on each save install nodemon by 'npm -D nodemon'
+ * use 'npm run dev' to start server if defined  "start": "node index",
+ *  "dev": "nodemon index"
  */
 
 //importing http,path and file system fs module
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-
-//creating port constant with port number
-const port = process.env.PORT || 8000; // will look for run time port on actual web server or use default for localhost
 
 //creating server and configuting
 const server = http.createServer((req, res) => {
@@ -33,20 +33,50 @@ const server = http.createServer((req, res) => {
     case ".js":
       contenttype = "text/javascript";
       break;
-      case ".css":
-      contenttype = "text/stylesheet";
+    case ".css":
+      contenttype = "text/css";
       break;
-      case ".json":
+    case ".json":
       contenttype = "application/json";
       break;
-      case ".png":
+    case ".png":
       contenttype = "image/png";
       break;
-      case ".jpg":
+    case ".jpg":
       contenttype = "image/png";
       break;
+    
   }
-  res.end();
+
+  //read file
+  fs.readFile(filepath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        //page not found!
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            if (err) throw err;
+            //set respose values
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        //some server error
+        res.writeHead(500);
+        res.end(`Server Error:${err.code}`);
+      }
+    } else {
+      //load page on success
+      res.writeHead(200, { "Content-Type": contenttype });
+      res.end(content, "utf8");
+    }
+  });
 });
+
+//creating port constant with port number
+const port = process.env.PORT || 8000; // will look for run time port on actual web server or use default for localhost
+
 // event listener
 server.listen(port, () => console.log("server is running.."));
