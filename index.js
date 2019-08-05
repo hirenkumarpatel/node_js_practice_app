f88e49a7a704ddfb230d3f19ffce8c7d1629c6ef
Hiren Patel <hirenkumarbpatel@gmail.com>
2019-08-05 17:09:24 -0400

    dynamic file path and content type check

--- index.js
+++ index.js
@@ -11,52 +11,42 @@ const fs = require("fs");
 const port = process.env.PORT || 8000; // will look for run time port on actual web server or use default for localhost
 
 //creating server and configuting
-const server= http
-  .createServer((req, res) => {
-    //writing respose (actual output to browser)
+const server = http.createServer((req, res) => {
+  //writing respose (actual output to browser)
 
-    // req.url will return current url in browser
-    if (req.url === "/") {
-      // if its just domain name with no trailing links
+  //create dynamic file path
+  let filepath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  console.log(filepath);
 
-      fs.readFile(
-        path.join(__dirname, "public", "index.html"),
-        (err, content) => {
-          if (err) throw err;
-          //setting respose values and write html content
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(content);
-        }
-      );
-    }
+  //extension of file
+  let extname = path.extname(filepath);
 
-    if (req.url === "/about") {
-          fs.readFile(
-          path.join(__dirname, "public", "about.html"),
-          (err, content) => {
-            if (err) throw err;
-            //setting respose values and write html content
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content);
-          }
-        );
-      }
+  //Initial Content type
+  let contenttype = "text/html";
 
-      //work as API
-      if (req.url === "/api/users") {
-        
-        //creating array of object (normally fetch from database or another site)
-        const users=[
-            {name:'Hiren Patel',age:29},
-            {name:'John Doe',age:35},
-            {name:'Will Smith',age:43}
-            
-        ];
-        //setting respose values and write html content
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(users));
-        }
-    });
-
-    // event listener
-    server.listen(port, () => console.log("server is running.."));
+  //check extension and set content-type
+  switch (extname) {
+    case ".js":
+      contenttype = "text/javascript";
+      break;
+      case ".css":
+      contenttype = "text/stylesheet";
+      break;
+      case ".json":
+      contenttype = "application/json";
+      break;
+      case ".png":
+      contenttype = "image/png";
+      break;
+      case ".jpg":
+      contenttype = "image/png";
+      break;
+  }
+  res.end();
+});
+// event listener
+server.listen(port, () => console.log("server is running.."));
---
+++ index_with_static_url.js
@@ -0,0 +1,60 @@
+/**
+ * creating basic server and start running server
+ */
+
+//importing http,path and file system fs module
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+//creating server and configuting
+const server = http.createServer((req, res) => {
+  //writing respose (actual output to browser)
+
+  // req.url will return current url in browser
+  // ===== static path=======
+  if (req.url === "/") {
+    // if its just domain name with no trailing links
+
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        //setting respose values and write html content
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        //setting respose values and write html content
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  //work as API
+  if (req.url === "/api/users") {
+    //creating array of object (normally fetch from database or another site)
+    const users = [
+      { name: "Hiren Patel", age: 29 },
+      { name: "John Doe", age: 35 },
+      { name: "Will Smith", age: 43 }
+    ];
+    //setting respose values and write html content
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+//creating port constant with port number
+const port = process.env.PORT || 8000; // will look for run time port on actual web server or use default for localhost
+
+// event listener
+server.listen(port, () => console.log("server is running.."));
