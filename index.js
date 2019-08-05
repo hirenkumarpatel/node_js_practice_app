916c8d74a5d0a3393658586c9d26ebdf168e8e7f
Hiren Patel <hirenkumarbpatel@gmail.com>
2019-08-05 15:58:11 -0400

    http basic server modified

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-//importing (object) defined in other file
-const person= require('./person_object');
-console.log(person);
+/**
+ * creating basic server and start running server
+ */
 
-//importing class defined in other file
-const Person= require('./person_class');
+//importing http module
+const http=require('http');
+const path=require('path');
+const fs=require('fs');
+//creating port constant with port number
+const port= process.env.PORT || 8000;// will look for run time port on actual web server or use default for localhost
 
-//creatning instance
-const hiren=new Person('Hirenkumar Patel',30);
 
-//calling get_data method
-console.log(hiren.get_data());
+//creating server and configuting 
+http.createServer((req,res)=>{
+    //writing respose (actual output to browser) 
+
+    // req.url will return current url in browser
+    if(req.url==='/'){ // if its just domain name with no trailing links 
+        
+        res.write('<h1>Home</h1>');
+    } 
+    
+
+    res.end();
+}).listen(port,()=>console.log('server is running..'));
--- reference/http_demo.js
+++ reference/http_demo.js
@@ -4,12 +4,21 @@
 
 //importing http module
 const http=require('http');
+
 //creating port constant with port number
-const port=8000;
+const port= process.env.PORT || 8000;// will look for run time port on actual web server or use default for localhost
+
 
 //creating server and configuting 
 http.createServer((req,res)=>{
-    //writing respose
-    res.write('Hello World!');
+    //writing respose (actual output to browser) 
+
+    // req.url will return current url in browser
+    if(req.url==='/'){ // if its just domain name with no trailing links 
+        
+        res.write('<h1>Home</h1>');
+    } 
+    
+
     res.end();
-}).listen(port,()=>console.log('server started'));
+}).listen(port,()=>console.log('server is running..'));
